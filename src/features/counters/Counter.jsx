19b683df32912cf680618342129f4969d15223a2
kssreelakshmi <kssreelakshmi2211@gmail.com>
2025-02-05 17:46:14 +0530

    updation

--- src/features/counters/Counter.jsx
+++ src/features/counters/Counter.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import {useSelector, useDispatch} from 'react-redux'
-import {increment, decrement,reset} from './counterSlice.jsx'
+import {increment, decrement,reset,incrementByAmount} from './counterSlice.jsx'
+import { useState } from 'react'
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count)
     const dispatch = useDispatch()
+    const [incrementAmount, setIncrementAmount] = useState(0)
+    const incrementValue = Number(incrementAmount) || 0 ;
+
+    const resetAll = () => {
+        dispatch(reset())
+        setIncrementAmount(0)
+    }
 
   return (
     <>
@@ -17,7 +25,11 @@ const Counter = () => {
             <button className='bg-black text-white size-12 rounded-lg mx-12'onClick={()=>dispatch(reset())}>reset</button>
             <button className='bg-black text-white size-10 rounded-lg mx-12' onClick={()=> dispatch(decrement())}>--</button>
         </div>
-
+        <input className='mx-12 my-12 border-2 text-center ' type="text" value={incrementAmount} onChange={(e)=>setIncrementAmount(e.target.value)}/>
+        <div className='flex justify-center items-center'>
+            <button className='bg-black text-white w-[115px] h-[50px] rounded-lg mx-12' onClick={(e)=>dispatch(incrementByAmount(incrementValue))}>Add Number</button>
+            <button className='bg-black text-white w-[115px] h-[50px] rounded-lg mx-12' onClick={(e)=>resetAll()}>Reset All Number</button>
+        </div>
         </div>
     </div>
     
